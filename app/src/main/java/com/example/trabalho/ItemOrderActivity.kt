c49fd4ec4be51294e4371abbb8043e55fa2835ac
Samira <sasalima609@gmail.com>
2025-04-07 02:12:30 -0300

    Criação do popup de confirmação de pedido

--- app/src/main/java/com/example/trabalho/ItemOrderActivity.kt
+++ app/src/main/java/com/example/trabalho/ItemOrderActivity.kt
@@ -1,11 +1,33 @@
 package com.example.trabalho
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Dialog
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class ItemOrderActivity : AppCompatActivity() {
+
+    private lateinit var popup: Button
+    private lateinit var dialog: Dialog
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_item_order)
+
+        popup = findViewById(R.id.btnConfirmarPedido)
+
+        popup.setOnClickListener {
+            dialog = Dialog(this)
+            dialog.setContentView(R.layout.activity_popup_confirmar_pedido)
+            dialog.setCancelable(true)
+            dialog.show()
+
+            // Fecha o dialog após 3 segundos (3000 milissegundos)
+            android.os.Handler().postDelayed({
+                if (dialog.isShowing) {
+                    dialog.dismiss()
+                }
+            }, 3000)
+        }
     }
 }
